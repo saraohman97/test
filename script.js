6316611ac93bcb122ddfb18bd52390606905ff7f
saraohman97 <saraohman97@gmail.com>
2022-02-03 21:29:42 +0100

    ngt fel

---
+++ script.js
@@ -0,0 +1,22 @@
+* {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+}
+
+html, body {
+    background-color: rgb(212, 212, 212);
+    font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+}
+
+.wrapper {
+    height: 100vh;
+    width: 100%;
+    display: flex;
+    justify-items: center;
+    align-items: center;
+}
+
+#regForm {
+    background-color: honeydew;
+}
