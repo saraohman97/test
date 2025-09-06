2f47e5abb8dcf820f328afd0b686c3432891297a
Sara Öhman <saraohman97@gmail.com>
2025-09-06 09:09:06 +0200

    What if I remove calling of data.

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,13 @@
-import getWines from "@/actions/getWines";
+// import getWines from "@/actions/getWines";
 import Form from "@/components/formpage";
-import WinesPage from "./(components)/client";
+// import WinesPage from "./(components)/client";
 
 export default async function Home() {
-  const wines = await getWines()
+  // const wines = await getWines()
 
   return (
     <div className="p-20">
-      <WinesPage wines={wines} />
+      {/* <WinesPage wines={wines} /> */}
       <br /> <br />
       <Form />
     </div>
